db05db0cbb201e83613eadc6e0261557645dd033
Ben Parisot <me@benparisot.com>
2019-04-10 13:27:37 -0700

    shorten transform function

---
+++ promise.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const readFilePromise = new Promise((resolve, reject) => {
+    //resolve = if the function resolves properly with resolve(data);
+    //reject(err)
+});
--- transform.js
+++ transform.js
@@ -3,20 +3,10 @@ const fsPromises = require('fs').promises;
 function transform(src) {
   return fsPromises.readFile(src, 'utf8')
     .then(data => {
-      const pattern = /[a-z]+/g;
-      const match = data.match(pattern);
-      const lowercaseString = match.join(' ');
-      return lowercaseString;
+      return data.replace(/[A-Z]+/g, '');
     })
-    .then(data => {
-      const uppercaseString = data.toUpperCase();
-      return uppercaseString;
-    })
-    .then(data => {
-      const splitDataArray = data.split('');
-      const reversedDataArray = splitDataArray.reverse();
-      return reversedDataArray.join('');
-    });
+    .then(data => data.toUpperCase())
+    .then(data => [...data].reverse().join(''));
 }
 
 module.exports = transform;
