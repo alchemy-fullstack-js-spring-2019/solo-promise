e06703ff2dbc913178a7d6ec98d8ad69a53c3976
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-10 14:09:37 -0700

    getting 1 futurama quote with superagent

--- test/copyFileFn.test.js
+++ test/copyFileFn.test.js
@@ -4,7 +4,12 @@ const fsPromises = require('fs').promises;
 
 describe('tests copy function', () => {
   it('passes a source and destination path into the copy function, tests that the contents of the copied file are the same as that of the original', () => {
-    copy('./lib/write.txt', './lib/write-copy.js')
-      .then(expect(fsPromises.readFile('./lib/write-copy.js'))).toBeTruthy();
+    return copy('./lib/write.txt', './lib/write-copy.js')
+      .then(() => {
+        return fsPromises.readFile('./lib/write-copy.js', { encoding: 'utf8' })
+          .then(result => {
+            expect(result).toEqual(fsPromises.readFile('./lib/write.txt'));
+          });
+      });
   });
 });
