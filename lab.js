3beac67194f615c24b6d7d2837807c627efa91e2
ollinieminen <otn987@gmail.com>
2019-04-10 11:18:41 -0700

    write a file works

--- lab.js
+++ lab.js
@@ -3,7 +3,21 @@
 
 const fsPromises = require('fs').promises;
 
-fsPromises.readFile('./1_promises.md', {encoding: 'utf8'})
-    .then(data=>{
-        console.log(data);
+fsPromises.readFile('./1_promises.md', { encoding: 'utf8' })
+    .then(data => {
+        //console.log(data);
     });
+
+//WRITE A FILE
+
+fsPromises.writeFile('./writeTest', 'why hello there!')
+.then(err => {
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log('DONE~!!!!!!');
+    }
+})
