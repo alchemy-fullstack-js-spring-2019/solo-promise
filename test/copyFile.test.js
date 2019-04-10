a56ef29eeb5ff5c7339a0de49a58968dae9e37b9
emily baier <emilyrbaier@gmail.com>
2019-04-10 12:00:22 -0700

    copy function reads file, test passes

---
+++ test/copyFile.test.js
@@ -0,0 +1,14 @@
+const copy = require('../lib/copyFile');
+const fsPromises = require('fs').promises;
+
+describe('copy file test', ()=> {
+    it('function read a file and copy it', ()=> {
+        return copy('./writtenfile.txt', './writtenfileCOPY.txt')
+            .then(()=> {
+                return fsPromises.readFile('./writtenfileCOPY.txt', { encoding: 'utf8' });
+            })
+            .then(data => {
+                expect(data).toEqual('i wrote this amazing file. so great!');
+            });
+    });
+});
