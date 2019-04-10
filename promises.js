cfc1619459fde566f6a9e398526fc1011143f395
Laura Badgley <l.badgleyy@gmail.com>
2019-04-10 11:18:28 -0700

    copying file huzzah!

--- promises.js
+++ promises.js
@@ -2,6 +2,7 @@ const fsPromise = require('fs').promises;
 
 fsPromise.readFile('./1_promises.md', { encoding: 'utf8' })
   .then(data => {
-    fsPromise.writeFile('./copy1_promise.md', 'NEW MESSAGE NEW FILE BIIIISH', { encoding: 'utf8' });
-    console.log('DONE');
+    fsPromise.writeFile('./copy1_promise2.md', data, { encoding: 'utf8' });
+    console.log(data, 'DONE');
   });
+ 
