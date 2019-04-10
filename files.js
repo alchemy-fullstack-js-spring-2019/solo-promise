9d1d2fba156077000cf6cf7233cc1c8106aaf479
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 13:45:12 -0700

    returned readFile promise

--- files.js
+++ files.js
@@ -1,23 +1,46 @@
 const fsPromises = require('fs').promises;
+const fs = require('fs');
 
-fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }).then(data => {
-  return data;
+// const readFilePromise = new Promise((reslove, reject) => {
+//   fs.readFile('./1_promises.md', { encoding: 'utf8' }, (err, data) => {
+//     if(err) return reject(err);
+//     resolve(data);
+//   });
+// });
 
-})
-  .then(data => console.log(data))
-  .catch(err => {
-    console.log(err);
+function readFilePromise2() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./1_promises.md', { encoding: 'utf8' }, (err, data) => {
+      if(err) return reject(err);
+
+      resolve(data);
+    });
+  });
+}
+
+readFilePromise2('./1_promises.md')
+  .then(data => console.log('yay', data))
+  .catch(err => console.log('nooo', err));
+
+
+// fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }).then(data => {
+//   return data;
+
+// })
+//   .then(data => console.log(data))
+//   .catch(err => {
+//     console.log(err);
 
     
-  });
+//   });
 
-const dataData = fsPromises.readFile('./1_promises.md', { encoding: 'utf8 '});
+// const dataData = fsPromises.readFile('./1_promises.md', { encoding: 'utf8 '});
 
-fsPromises.writeFile('./stuff.text', 'write somethuing and try not to be obnoxious', { encoding: 'utf8' }).then(data => {
-  return data;
-}).then(data => console.log('saved', data)).catch(err => {
-    console.log('nope', err);
-});
+// fsPromises.writeFile('./stuff.text', 'write somethuing and try not to be obnoxious', { encoding: 'utf8' }).then(data => {
+//   return data;
+// }).then(data => console.log('saved', data)).catch(err => {
+//   console.log('nope', err);
+// });
     
 // (err) => {
 //   if(err) throw err;
@@ -31,9 +54,14 @@ fsPromises.writeFile('./stuff.text', 'write somethuing and try not to be obnoxio
 
 // .then(([promises]));
 
-Promise.resolve({
-  name: 'spot',
-  age: 10
-});
+// Promise.resolve({
+//   name: 'spot',
+//   age: 10
+// });
 
-
+// fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }).then(data =>{
+//   return data.replace(/A-Z/g, '');
+// }) //
+//   .then(data => data.toUpperCase())
+//   .then(data => [...allCaps].reverse().join(''))
+//   .then(console,log);
