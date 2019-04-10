2e4839679dea031e7223ce4b9525ac280b1d6d40
Cheri Portman <cheriportman@hotmail.com>
2019-04-10 11:08:53 -0700

    writeFile promise task complete

--- files.js
+++ files.js
@@ -4,3 +4,8 @@ fsPromises.readFile('./1_promises.md', { encoding: 'utf8' })
   .then(data => {
     console.log(data);
   });
+
+fsPromises.writeFile('write.md', 'this is a test')
+  .then(data => {
+    console.log('done');
+  });
