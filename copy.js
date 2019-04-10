9502c39a00d1e7346ae6b1df90f93332562cb73b
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 16:58:02 -0700

    fixed copy.js

--- copy.js
+++ copy.js
@@ -1,5 +1,8 @@
 const fsPromises = require('fs').promises;
 
-function copy(src, data) {
-    return fsPromises.readFile
+function copy(src, destination) {
+  return fsPromises.readFile(src)
+    .then(data => fsPromises.writeFile(destination, data));
 }
+
+module.exports = copy;
