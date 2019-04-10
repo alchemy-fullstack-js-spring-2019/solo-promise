abf613ef74b894d7ee67d1fc87ef9c7396f493b5
Cara Ponzini <ponzini@gmail.com>
2019-04-10 14:48:07 -0700

    returned info for planets

---
+++ superagent.js
@@ -0,0 +1,30 @@
+const request = require('superagent');
+const fsPromises = require('fs').promises;
+
+// var superA = new request();
+// request.get('http://futuramaapi.herokuapp.com/api/quotes/3')
+//   .then(res => res.body)
+//   .then(result => console.log(result));
+
+// request.get('futuramaapi.herokuapp.com/api/characters/Bender')
+//   .then(res => res.body)
+//   .then(result => console.log(result));
+
+request.get('https://rickandmortyapi.com/api/character/')
+  .then(results => results.body)
+  .then(results => {
+    return results.results.map(result => result.origin.url);
+  })
+  .then(urlArray => {
+    return urlArray.filter(entry => entry !== '');})
+  .then(allUrls => [...new Set(allUrls)]) //remove dulpicates
+  .then(urls => {
+    return Promise.all(
+      urls.map((url) => {
+        return request.get(url).then(res => res.body);
+      })
+    );
+  })  
+  .then(locations => console.log(locations));
+
+
