e7fb84a8cf7224776346e205d99102052c48b21d
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-10 15:56:57 -0700

    rick and morty functions etc

--- rickAndMorty.js
+++ rickAndMorty.js
@@ -1,5 +1,18 @@
 const request = require('superagent');
 
+function getOriginForCharacters(characters) {
+    const dedupedUrl = [...new Set(characters
+        .map(character => character.origin.url))]
+    return Promise.all(
+        dedupedUrl
+            .filter(url => url !== '')
+            .map(url => request.get(url).then(res => res.body))
+    );
+}
+
+getOriginForCharacters();
+
+
 request
     .get('https://rickandmortyapi.com/api/character/')
     .then(res => {
