b0f7bfb1c7c21170f2d23b81abf45811b964bfbf
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 14:34:36 -0700

    made get quote function

--- futurama.js
+++ futurama.js
@@ -1,7 +1,19 @@
 const request = require('superagent');
 
-request
-  .get('http://futuramaapi.herokuapp.com/api/quotes/1')
-  .then(res => 
-    res.body)
+
+// request
+//   .get('http://futuramaapi.herokuapp.com/api/quotes/1')
+//   .then(res => 
+//     res.body)
+//   .then(console.log);
+
+function getQuote() {
+  return request
+    .get('http://futuramaapi.herokuapp.com/api/quotes/1')
+    .then(res => 
+      res.body);
+    // .then(console.log);
+}
+
+getQuote()
   .then(console.log);
