200b22ddbbb4e6d43920fe2cc8d11ed4a42cd790
Sean Nelson <smnelsonn@gmail.com>
2019-04-10 15:28:47 -0700

    getCharacter function working

--- test/rickAndMortyApi.test.js
+++ test/rickAndMortyApi.test.js
@@ -3,7 +3,14 @@ const { getCharacter } = require('../lib/rickAndMortyApi.js');
 describe('service tests', () => {
   const id = 5;
 
-  it('returns char details', {
-    expect(getCharacter(id)).toEqual();
+  it('returns character', () => {
+    return getCharacter(id)
+      .then(data => {
+        expect(data).toEqual({
+          name: 'Jerry Smith',
+          status: 'Alive',
+          species: 'Human'
+        });
+      });
   });
 });
