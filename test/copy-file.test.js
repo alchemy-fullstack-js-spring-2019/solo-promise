275d947865016055bbd3cd5cb0534d892c263275
mcnadams <mcneilbo@oregonstate.edu>
2019-04-10 13:09:30 -0700

    TDD copy function

--- test/copy-file.test.js
+++ test/copy-file.test.js
@@ -1,8 +1,14 @@
 const copy = require('../lib/copy-file');
+const fsPromises = require('fs').promises;
 
 describe('copy a file using promises', () => {
     it('copies src file to dest', () => {
-        copy('../promise-copy.md', 'test-promise-copy.md');
-        //expect('./test-promise-copy.md').toBeAnExistingFile();
+        return copy('./promise-copy.md', './yet-another-promise-copy.md')
+            .then(() => {
+                return fsPromises.readFile('./yet-another-promise-copy.md', 'utf8');
+            })
+            .then(result => {
+                expect(result).toBeTruthy();
+            });      
     });
 });
