fcffa7dd439676ada384119062edb6892eab5ff7
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-10 16:22:37 -0700

    what in the what

--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -1,10 +1,16 @@
-const getCharacter = require('rickAndMortyApi.js');
+const { getCharacter } = require('./rickAndMortyApi');
+
 
 describe('getCharacter function tests', () => {
   it('takes an int id and returns promise to resolve character', () => {
     return getCharacter(12)
-      .then()
-
+      .then(character => {
+        expect(character).toEqual({
+          name: 'Alexander',
+          species: 'Human',
+          status: 'Dead'
+        });
+      });
 
   });
 });
