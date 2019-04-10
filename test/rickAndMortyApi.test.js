334d83d6f4abf3c9316a80b07155482242a1b089
Tommy Tran <tranttommy95@gmail.com>
2019-04-10 15:34:42 -0700

    getCharacter function passed

---
+++ test/rickAndMortyApi.test.js
@@ -0,0 +1,24 @@
+const getCharacter = require('../lib/rickAndMortyApi.js');
+
+describe('getCharacter', () => {
+  it('returns a morty character object containing only character\'s name, status, and species from id', () => {
+    const expected = {
+      name: 'Morty Smith',
+      status: 'Alive',
+      species: 'Human'
+    };
+    const id = 2;
+    return getCharacter(id)
+      .then(result => expect(result).toEqual(expected));
+  });
+  it('returns a rick character object containing only character\'s name, status, and species from id', () => {
+    const expected = {
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human'
+    };
+    const id = 1;
+    return getCharacter(id)
+      .then(result => expect(result).toEqual(expected));
+  });
+});
