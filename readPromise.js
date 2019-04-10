c56af3290446131e1157d97c7d69c5036cad712b
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-10 13:32:22 -0700

    readPromise function

---
+++ readPromise.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const options = { encoding: 'utf8' };
+
+
+function readPromise(src) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(src, options, (err, data) =>{
+      if(err) return reject(err);
+
+      resolve(data);
+    });
+  });
+}
+
+
+readPromise('./write-by-promise.txt')
+  .then(console.log);
