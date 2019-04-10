1c1206217cd829428b40a9ec5a797b342293ff8d
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-10 11:08:39 -0700

    write and copy working

--- promises.js
+++ promises.js
@@ -4,8 +4,17 @@ const options = {
   encoding: 'utf8'
 };
 
-const read = () => new Promise((resolve) => {
-  resolve(fsPromises.readFile('./1_promises.md', options));
-});
+// fsPromises.readFile('./1_promises.md', options)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-read().then(data => console.log(data));
+// fsPromises.writeFile('./write1.txt', 'hi there', options)
+//   .then(() => console.log('DONE!'))
+//   .catch(err => console.log(err));
+
+fsPromises.readFile('./1_promises.md', options)
+  .then(data => {
+    fsPromises.writeFile('./1_promises-copy.md', data, options);
+  })
+  .then(() => console.log('DONE!'))
+  .catch(err => console.log(err));
