2abb26ca8633ab7a93ed3b523b14c3042a4debf6
Emily Creamer <emily.creamer@icloud.com>
2019-04-10 13:35:02 -0700

    copy test passing

--- test/copy.test.js
+++ test/copy.test.js
@@ -3,16 +3,19 @@ const copy = require('../lib/copy');
 
 describe('promises copy function', () => {
   afterEach(() => {
-    return fsPromises.unlink('./copy.txt');
+    return fsPromises.unlink('./copy-test.txt');
   });
 
   it('can copy a file', () => {
-    return copy('./test.txt', './copy.txt')
+    return copy('./test.txt', './copy-test.txt')
     .then(() => {
-      return fsPromises.readFile('./copy.txt', { encoding: 'utf8' });
+      return Promise.all([
+        fsPromises.readFile('./test.txt', { encoding: 'utf8' }),
+        fsPromises.readFile('./copy-test.txt', { encoding: 'utf8' })
+      ]);
     })
-    .then(result => {
-      expect(result).toEqual('hi\n');
-    })
+    .then(([expected, result]) => {
+      expect(expected).toEqual(result);
+    });
   });
 }); 
