21dd17393f1f3fa140304b0e383bfa1a8610066f
mcnadams <mcneilbo@oregonstate.edu>
2019-04-10 13:58:48 -0700

    read promise

--- test/transform.test.js
+++ test/transform.test.js
@@ -4,7 +4,6 @@ describe('transform file text', () => {
     it('transforms the contents of a file', () => {
         return transform('./lib/dog.md')
             .then(result => {
-                console.log('result', result);
                 expect(result).toEqual('\n!WOW WO !FOOW FOO');
             });
     });
