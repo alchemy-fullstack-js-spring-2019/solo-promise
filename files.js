627f98e8a575b7df60a2df48eb4986458365e297
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 11:55:41 -0700

    can write to a file

--- files.js
+++ files.js
@@ -7,7 +7,21 @@ fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }).then(data => {
   .then(data => console.log(data))
   .catch(err => {
     console.log(err);
+
+    
   });
+
+
+
+fsPromises.writeFile('./stuff.text', 'there is a lot of things i could write to be obnoxious', { encoding: 'utf8' }).then(data => {
+  return data;
+}).then(data => console.log('saved', data)).catch(err => {
+    console.log('nope', err);
+});
+    
+// (err) => {
+//   if(err) throw err;
+//   console.log('saved');
 
 // Promise.all([
 //   fsPromises.readFile('/1_promises.md', { encoding: 'utf8' }),
@@ -18,6 +32,6 @@ fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }).then(data => {
 // .then(([promises]));
 
 Promise.resolve({
-    name: 'spot',
-    age: 10
+  name: 'spot',
+  age: 10
 });
