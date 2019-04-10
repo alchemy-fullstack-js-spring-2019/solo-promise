af6e54fc1e7fb66a007dfe1d00f72531b4ad8914
UncleJerry <jared.myhrberg@gmail.com>
2019-04-10 14:11:12 -0700

    transform working

--- test/transform.test.js
+++ test/transform.test.js
@@ -1,11 +1,10 @@
-const fsPromises = require('fs').promises;
 const transform = require('../lib/transform');
 
 describe('transform fn', () => {
   it('does funny things to file contents', () => {
     return transform('./letters.txt')
       .then(data => {
-        expect(data).toEqual('EREHT OLLOH');
+        expect(data).toEqual('\nEREHT OLLEH');
       });
   });
 });
