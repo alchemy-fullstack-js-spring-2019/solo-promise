ceff156fe78d0fb290817b7550d98df42b627898
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 16:54:44 -0700

    rickMortyApi fixed

--- rickMortyApi.js
+++ rickMortyApi.js
@@ -1,17 +1,18 @@
 const request = require('superagent');
 
 function getOriginCharacters(characters) {
+  const deduplicateUrls = [...new Set(characters.map(character => character.origin.url))];
+  return Promise.all(
+    deduplicateUrls
+      .filter(url => url !== '')
+      .map(url => request.get(url))
+  );
 
 }
 
 request
   .get('https://rickandmortyapi.com/api/character/')
   .then(res => res.body.results)
-  .then(characters => characters.map(character => character.origin.url))
-  .then(characterlocationUrls => characterlocationUrls.filter(url => url !== ''))
-  .then(characterlocationUrls => {
-    return Promise.all(characterlocationUrls.map(url => {
-      return request.get(url).then(res => res.body);
-    }))
-      .then(console.log);
-  });
+  .then(getOriginCharacters);
+  
+
