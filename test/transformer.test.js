553f2273222e6798983479753601a1248af80031
Tommy Tran <tranttommy95@gmail.com>
2019-04-10 14:19:20 -0700

    readPromise and test passed

--- test/transformer.test.js
+++ test/transformer.test.js
@@ -2,10 +2,10 @@ const transformer = require('../lib/transformer');
 
 describe('transformer function', () => {
   it('it reads a file, then removes all capital letters, then makes all letters capital,then reverses the string', () => {
-    const expected = 'DEIL EH ESUACEB KO S\'EH';
+    const expected = '\nDEIL EH ESUACEB KO S\'EH';
     return transformer('transformer.txt')
       .then(transformed => {
-        expect(transformed).toContain(expected);
+        expect(transformed).toEqual(expected);
       });
   });
 });
