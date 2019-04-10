01c4d9aeb3dd443a121250fae51c745c4c5dddc5
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-10 11:37:39 -0700

    copy file function written, test passing

--- tests/copyFile.test.js
+++ tests/copyFile.test.js
@@ -1,10 +1,12 @@
 const copyFile = require('../lib/copyFileFunction'
 );
+const fsPromises = require('fs').promises;
 
 describe('copyFile function', () => {
     it('copies a file', () => {
-        const oldFile = '../1_promises.md';
-        const newFile = copyFile(oldFile);
-        expect(oldFile).toEqual(newFile);
-    }
-})
+        const oldFile = './1_promises.md';
+        const newFile = './1_promises_3.md';
+        const result = copyFile(oldFile, newFile);
+        expect(fsPromises.readFile(result)).toEqual(fsPromises.readFile(oldFile));
+    });
+});
