291c0f3b5fc118a4fb68b3b70704668bea7c0a32
emily baier <emilyrbaier@gmail.com>
2019-04-10 14:11:53 -0700

    copy file and transform file functions and tests

--- test/copyFile.test.js
+++ test/copyFile.test.js
@@ -2,13 +2,18 @@ const copy = require('../lib/copyFile');
 const fsPromises = require('fs').promises;
 
 describe('copy file test', ()=> {
-    it('function read a file and copy it', ()=> {
+
+    // afterEach(()=> 
+    //     fsPromises.unlink('./writtenfileCOPY.txt')
+    // );
+
+    it('function read a file and copy it', () => {
         return copy('./writtenfile.txt', './writtenfileCOPY.txt')
             .then(()=> {
                 return fsPromises.readFile('./writtenfileCOPY.txt', { encoding: 'utf8' });
             })
             .then(data => {
-                expect(data).toEqual('i wrote this amazing file. so great!');
+                expect(data).toEqual('I wrote this amazing file. so great!\n');
             });
     });
 });
