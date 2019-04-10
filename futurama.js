91b4022331f0acc718bfc7641588ff25e39fece7
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 14:50:39 -0700

    poopyButthole function works

--- futurama.js
+++ futurama.js
@@ -7,24 +7,35 @@ const request = require('superagent');
 //     res.body)
 //   .then(console.log);
 
-function getQuote() {
+// function getQuote() {
+//   return request
+//     .get('http://futuramaapi.herokuapp.com/api/quotes/1')
+//     .then(res => 
+//       res.body);
+// }
+
+// getQuote()
+//   .then(console.log);
+
+// function getCharacterQuote() {
+//   return request
+//     .get(`http://futuramaapi.herokuapp.com/api/characters/${character}/${count}`)
+//     .then(res => 
+//       res.body);
+// }
+
+// getCharacterQuote('bender', 5)
+//   .then(console.log);
+
+function poopyButthole() {
   return request
-    .get('http://futuramaapi.herokuapp.com/api/quotes/1')
-    .then(res => 
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(res =>
       res.body);
 }
 
-getQuote()
-  .then(console.log);
+poopyButthole()
+  .then(console.log); 
 
-function getCharacterQuote() {
-  return request
-    .get('http://futuramaapi.herokuapp.com/api/characters/${character}/1')
-    .then(res => 
-      res.body);
-}
 
-getCharacterQuote('bender', 1)
-  .then(console.log);
 
-  
