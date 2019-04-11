6524e5a8dcfb8e744a7daba1b1c28407d699d3d7
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 17:21:04 -0700

    removed comments

--- files.js
+++ files.js
@@ -1,12 +1,5 @@
-const fsPromises = require('fs').promises;
 const fs = require('fs');
 
-// const readFilePromise = new Promise((reslove, reject) => {
-//   fs.readFile('./1_promises.md', { encoding: 'utf8' }, (err, data) => {
-//     if(err) return reject(err);
-//     resolve(data);
-//   });
-// });
 
 function readFilePromise2() {
   return new Promise((resolve, reject) => {
@@ -18,44 +11,6 @@ function readFilePromise2() {
   });
 }
 
-readFilePromise2('./1_promises.md')
-  .then(data => console.log('yay', data))
-  .catch(err => console.log('nooo', err));
-
-
-// fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }).then(data => {
-//   return data;
-
-// })
-//   .then(data => console.log(data))
-//   .catch(err => {
-//     console.log(err);
-
-    
-//   });
-
-// const dataData = fsPromises.readFile('./1_promises.md', { encoding: 'utf8 '});
-
-// fsPromises.writeFile('./stuff.text', 'write somethuing and try not to be obnoxious', { encoding: 'utf8' }).then(data => {
-//   return data;
-// }).then(data => console.log('saved', data)).catch(err => {
-//   console.log('nope', err);
-// });
-    
-// (err) => {
-//   if(err) throw err;
-//   console.log('saved');
-
-// Promise.all([
-//   fsPromises.readFile('/1_promises.md', { encoding: 'utf8' }),
-//   fsPromises.readFile('/package.json', { encoding: 'utf8' }),
-//   fsPromises.readFile('/files.js', { encoding: 'utf8' })
-// ]);
-
-// .then(([promises]));
-
-// Promise.resolve({
-//   name: 'spot',
-//   age: 10
-// });
+readFilePromise2('./1_promises.md');
+ 
 
