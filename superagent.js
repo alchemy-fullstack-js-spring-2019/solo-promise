25a1864e3df8bcfd9343c8398e2e932b90c9c2bc
Ben Parisot <me@benparisot.com>
2019-04-10 19:49:03 -0700

    finish superagent.js

--- superagent.js
+++ superagent.js
@@ -1,27 +1,32 @@
 const request = require('superagent');
 
-// request
-//   .get('https://rickandmortyapi.com/api/character/')
-//   .then(res => console.log(res.body));
+//get a promise with a list of characters
+request
+  .get('https://rickandmortyapi.com/api/character/')
+  .then(res => console.log(res.body));
 
-// request
-//   .get('futuramaapi.herokuapp.com/api/characters/bender/5')
-//   .then(res => console.log(res.body));
+//get a number of quotes from a certain character
+request
+  .get('futuramaapi.herokuapp.com/api/characters/bender/5')
+  .then(res => console.log(res.body));
 
+//use in request to get single character origins
+function getCharacterOrigin(characters) {
+  const setUrls = [...new Set(characters.map(character => character.origin.url))];
+  return Promise.all(
+    setUrls
+      .filter(url => url !== '')
+      .map(url => request.get(url)
+        .then(res => res.body.name)));
+}
+
+//initial request to get characters and call getCharacterOrigin on results
 request
   .get('https://rickandmortyapi.com/api/character')
   .then(res => res.body.results)
-  .then(characters => {
-    return characters.map(character => character.origin.url);
-  })
-  .then(characterLocationURLS => characterLocationURLS.filter(url => url !== ''))
-  .then(characterLocationURLS => {
-    return Promise.all(characterLocationURLS.map(url => {
-      return request.get(url).then(res => res.body);
-    }));
-  })
+  .then(getCharacterOrigin)
   .then(console.log);
-  
+
 
 
   
