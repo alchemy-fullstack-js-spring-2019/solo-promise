6a55a87f09477985c149a0a50c0baf0e3f8d44b4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-11 08:50:54 -0700

    rick and morty api

---
+++ tests/rickAndMortyApi.test.js
@@ -0,0 +1,23 @@
+const { getCharacter } = require('../lib/services/rickAndMortyApi');
+
+jest.mock('../lib/services/rickAndMortyApi.js', () => ({
+    getCharacter() {
+        return Promise.resolve({
+            name: 'Morty Smith',
+            species: 'Human',
+            status: 'Alive'
+        });
+    }
+}));
+describe('rick and morty service', () => {
+    it('can get a character by id', () => {
+        return getCharacter(1)
+            .then(character => {
+                expect(character).toEqual({
+                    name: 'Morty Smith',
+                    species: 'Human',
+                    status: 'Alive'
+                });
+            });
+    });
+});
