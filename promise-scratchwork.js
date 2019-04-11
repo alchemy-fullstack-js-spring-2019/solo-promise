590ab1d8431cf7686fe99b0561e635f29e3ef3e2
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-10 17:53:06 -0700

    just some house keeping

--- promise-scratchwork.js
+++ promise-scratchwork.js
@@ -18,20 +18,17 @@ fsPromises.readFile('./1_promises.md', { encoding: 'utf8'})
         console.error(err);
     });
 
-    //first promise to resolve moves to next step
     Promise.race([
         fsPromises.readFile('./1_promises.md', { encoding: 'utf8'}),
         fsPromises.readFile('./package.json', { encoding: 'utf8'})
     ])
         .then(data => console.log(data));
 
-    //become array of fulfilled value, promises must all fulfill before moving
     Promise.all([
         fsPromises.readFile('./1_promises.md', { encoding: 'utf8'}),
         fsPromises.readFile('./package.json', { encoding: 'utf8'})
     ])
         .then(([promisesMd, packageJson]) => console.log(promisesMd));
-    //waits for each to resolve then receive array of fulfilled promises
 
     Promise.resolve(1)
         .then(console.log);
