e81274d91a00f63de5f5e4531da509a47c5898a1
Peter Kotowski <bighoopee@gmail.com>
2019-04-10 17:11:56 -0700

    copy.test

--- copy.test.js
+++ copy.test.js
@@ -3,16 +3,24 @@ const fsPromises = require('fs').promises;
 
 
 describe('copy function', () =>{
-    afterEach(() => {
-        return fsPromises.unlink('../stuff.text');
-    });
+  afterEach(() => {
+    return fsPromises.unlink('../stuff.text');
+  });
 
-    it('can copy a file', () => {
-return copy('./stuff.text', './somethiing.txt').then(() => {
-    return fsPromises.readFile('./stuff.text', { encoding: 'utf8' }).then(result => {
-        expect
-    });
+  it('can copy a file', () => {
+    return copy('./stuff.text', './something.txt')
+      .then(() => {
+        return Promise.all([
+          fsPromises.readFile('./stuff.text', { encoding: 'utf8' }),
+          fsPromises.readFile('./something.txt', { encoding: 'utf8'})
+        ]);
+        
+      })
+      .then(([expected, result]) => {
+        expect(expected).toEqual(result);
+      });
+  });
 });
-    });
-});
+
+
 
