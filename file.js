6939c218208baa6ee23810e59c0431c1a50418a5
Laura Badgley <l.badgleyy@gmail.com>
2019-04-10 17:00:32 -0700

    commit

--- file.js
+++ file.js
@@ -1,39 +1,15 @@
 const fsPromises = require('fs').promises;
+const fs = require('fs');
 
-fsPromises.readFile('./1_promises.md', { encoding: 'utf8' })
-  .then(data => {
-
-    return 'hi';
-    // this return can also return another promise to read/write whatever with another file...
-    // return fs.Promises.readFile(....some file, some encoding...)
-  }) 
-  .then([promisesMd, packageJson]) => console.log(packageJson)
-  .catch(err => {
-    console.log(data);
-    console.error(err);
+function readFilePromise(src) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(src, { encoding: 'utf8' }, (err, data) => {
+      if(err) return reject(err);
+      resolve(data);
+    });
   });
+}
 
-Promise.race([
-  fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }),
-  fsPromises.readFile('./package.json', { encoding: 'utf8' }),
-  fsPromises.readFile('./file.js', { encoding: 'utf8' })
-
-])
-  .then(data => console.log(data));
-
-// sees which promise is fulfilled first (fastest)
-
-Promise.all([
-  fsPromises.readFile('./1_promises.md', { encoding: 'utf8' }),
-  fsPromises.readFile('./package.json', { encoding: 'utf8' }),
-  fsPromises.readFile('./file.js', { encoding: 'utf8' })
-])
-  .then(files => {
-    console.log(files[//file array position from above//])
-  }
-// waits for these to resolve and gives you the value?
-
-// OR
-// .then([promisesMD, packageJson, fileJs]) => {
-  // console.log(promisesMD)
-// };
+readFilePromise('./1_promises.md')
+  .then(data => console.log('success', data))
+  .catch(err => console.error('error', err));
