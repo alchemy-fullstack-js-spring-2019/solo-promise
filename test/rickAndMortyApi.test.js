98bf20b1ee0924745b3cf830e3a2d65febe9cf76
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-11 10:11:55 -0700

    rick and morty api passes tests

---
+++ test/rickAndMortyApi.test.js
@@ -0,0 +1,14 @@
+const getCharacter = require('../lib/rickAndMortyApi');
+
+describe('rick and morty api test', () => {
+  it('function returns name status and species of character specified by id', () => {
+    return getCharacter(1)
+      .then(res => {
+        expect(res).toEqual({
+          name: 'Rick Sanchez',
+          status: 'Alive',
+          species: 'Human',
+        });
+      });
+  });
+});
