8f719f58a98067510d228efa61a0ab437831b023
Cara Ponzini <ponzini@gmail.com>
2019-04-10 17:29:08 -0700

    updated promiseCheck

--- promiseCheck.js
+++ promiseCheck.js
@@ -1,10 +1,7 @@
 const request = require('superagent');
 
 function promiseCheck(toCheck) {
-  if(typeof toCheck === 'object' && typeof toCheck.then === 'function'){
-    return true;
-  }
-  else {return false;}
+  return toCheck instanceof Promise;
 }
 
 module.exports = promiseCheck;
