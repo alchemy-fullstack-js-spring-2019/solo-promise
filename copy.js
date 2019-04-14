5ee76c7ebb268dea268ee6308fbe4b463a0b6255
Masmics <cherlocker3113@gmail.com>
2019-04-14 16:04:57 -0700

    debugging copy function

--- copy.js
+++ copy.js
@@ -1,8 +1,8 @@
 const fsPromises = require('fs').promises;
-
+//const encoding = { encoding: 'utf8' };
 function copy(source, dest) {
-  return fsPromises.readFile(source)
-    .then(data => fsPromises.writeFile(dest, data));
+  return fsPromises.readFile(source, { encoding: 'utf8' })
+    .then(data => fsPromises.writeFile(dest, data, { encoding: 'utf8' }));
 }
 
 module.exports = copy;
