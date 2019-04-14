7d2d34dbd3f2a922b6f9da9187384c0d69713fa5
Masmics <cherlocker3113@gmail.com>
2019-04-14 16:56:34 -0700

    travis again

--- transform.js
+++ transform.js
@@ -1,7 +1,8 @@
 const fsPromises = require('fs').promises;
 
 function transform(src) {
-  return fsPromises.readFile(src, { encoding: 'utf8' })
+  let encoding = { encoding: 'utf8' };
+  return fsPromises.readFile(src, encoding)
     .then(data => data.replace(/[A-Z]/g, ''))
     .then(noCaps => noCaps.toLowerCase())
     .then(allCaps => allCaps.toUpperCase());
