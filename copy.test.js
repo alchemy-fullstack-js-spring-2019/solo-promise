75aa6be8aa4e9185287d642d2408daebc9d5d0f8
Masmics <cherlocker3113@gmail.com>
2019-04-14 16:23:31 -0700

    copy function debugged

--- copy.test.js
+++ copy.test.js
@@ -7,16 +7,26 @@ describe('copy function', () => {
     return fsPromises.unlink('./copy.txt');
   });
 
-  it('can copy a file', () => {
+  it('can copy a text file', () => {
+    return copy('./test.txt', './copy.txt')
+      .then(() => {
+        return fsPromises.readFile('copy.txt', { encoding: 'utf8' });
+      })
+      .then(data => {
+        expect(data).toContain('Test data for copy function.');
+      });
+  });
+
+  it('can copy a json file', () => {
     return copy('./package.json', './copy.txt')
       .then(() => {
         return Promise.all([
-          fsPromises.readFile('./test.txt', { encoding: 'utf8' }),
+          fsPromises.readFile('./package.json', { encoding: 'utf8' }),
           fsPromises.readFile('./copy.txt', { encoding: 'utf8' })
         ]);
       })
-      .then(() => {
-        expect('./copy.txt');
+      .then(data => {
+        expect(data.toString()).toContain('"devDependencies"');
       });
   });
 });
