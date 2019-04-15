8fdad7f96337f60fad2cb3271e29fc7e3af7878c
Masmics <cherlocker3113@gmail.com>
2019-04-14 17:31:56 -0700

    debugging with previous build

--- copy.js
+++ copy.js
@@ -1,8 +1,18 @@
-const fsPromises = require('fs').promises;
-//const encoding = { encoding: 'utf8' };
-function copy(source, dest) {
-  return fsPromises.readFile(source, { encoding: 'utf8' })
-    .then(data => fsPromises.writeFile(dest, data, { encoding: 'utf8' }));
-}
+// const fsPromises = require('fs').promises;
+// //const encoding = { encoding: 'utf8' };
+// function copy(source, dest) {
+//   return fsPromises.readFile(source, { encoding: 'utf8' })
+//     .then(data => fsPromises.writeFile(dest, data, { encoding: 'utf8' }));
+// }
 
-module.exports = copy;
+// module.exports = copy;
+
+const fsPromises = require('fs').promises;
+
+module.exports = (src, dst) => {
+
+  fsPromises.readFile(src)
+    .then(data => fsPromises.writeFile(dst, data));
+  // .then(console.log('copied it!'))
+  // .catch(err => console.error(err));
+};
--- copy.test.js
+++ copy.test.js
@@ -1,35 +1,59 @@
+// const fsPromises = require('fs').promises;
+
+// const copy = require('./copy.js');
+
+// describe('copy function', () => {
+
+//   afterEach(() => {
+//     // eslint-disable-next-line
+//     return fsPromises.unlink('./copy.txt');
+//   });
+
+//   it('can copy a text file', () => {
+//     return copy('./test.txt', './copy.txt')
+//       .then(() => {
+//         return fsPromises.readFile('./copy.txt', { encoding: 'utf8' });
+//       })
+//       .then(data => {
+//         expect(data).toContain('Test data for copy function.');
+//       });
+//   });
+
+//   it('can copy a json file', () => {
+//     return copy('./package.json', './copy.txt')
+//       .then(() => {
+//         return Promise.all([
+//           fsPromises.readFile('./package.json', { encoding: 'utf8' }),
+//           fsPromises.readFile('./copy.txt', { encoding: 'utf8' })
+//         ]);
+//       })
+//       .then(data => {
+//         expect(data.toString()).toContain('"devDependencies"');
+//       });
+//   });
+// });
+
+const copy = require('./copy');
 const fsPromises = require('fs').promises;
-
-
-const copy = require('./copy.js');
-
-describe('copy function', () => {
-
+//
+describe('copy', () => {
   afterEach(() => {
-    // eslint-disable-next-line
-    return fsPromises.unlink('./copy.txt');
+    return fsPromises.unlink('./http-copy.md');
   });
 
-  it('can copy a text file', () => {
-    return copy('./test.txt', './copy.txt')
+  it('copies from src and write it to dst', () => {
+    return copy('../http.md', '/http-copy.md')
       .then(() => {
-        return fsPromises.readFile('./copy.txt', { encoding: 'utf8' });
-      })
-      .then(data => {
-        expect(data).toContain('Test data for copy function.');
-      });
-  });
-
-  it('can copy a json file', () => {
-    return copy('./package.json', './copy.txt')
-      .then(() => {
+        //expect(fs.readFileSync('./.gitignore')).toEqual(fs.readFileSync('./test.txt))
+        
         return Promise.all([
-          fsPromises.readFile('./package.json', { encoding: 'utf8' }),
-          fsPromises.readFile('./copy.txt', { encoding: 'utf8' })
+          fsPromises.readFile('../http.md'),
+          fsPromises.readFile('./http-copy.md')
         ]);
       })
-      .then(data => {
-        expect(data.toString()).toContain('"devDependencies"');
-      });
+      .then(([readHttpMd, copyHttpMd]) => {
+        expect(readHttpMd).toEqual(copyHttpMd);
+      })
+      .catch(err => expect(err).toBeFalsy());
   });
 });
