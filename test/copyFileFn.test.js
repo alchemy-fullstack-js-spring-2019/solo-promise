2c1b7db9242c780ef8752ff930a02513258661e0
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-15 09:00:23 -0700

    copyFn passing

--- test/copyFileFn.test.js
+++ test/copyFileFn.test.js
@@ -4,12 +4,14 @@ const fsPromises = require('fs').promises;
 
 describe('tests copy function', () => {
   it('passes a source and destination path into the copy function, tests that the contents of the copied file are the same as that of the original', () => {
-    return copy('./lib/write.txt', './lib/write-copy.js')
+    return copy('./lib/write.txt', './lib/write-copy.txt')
       .then(() => {
-        return fsPromises.readFile('./lib/write-copy.js', { encoding: 'utf8' })
-          .then(result => {
-            expect(result).toEqual(fsPromises.readFile('./lib/write.txt'));
+        return Promise.all([fsPromises.readFile('./lib/write-copy.txt', { encoding: 'utf8' }), fsPromises.readFile('./lib/write.txt', { encoding: 'utf8' })])
+          .then(([result1, result2]) => {
+            expect(result1).toEqual(result2);
           });
       });
   });
 });
+
+
