c4faa4038f97d5f447a0d25c42054665d966fbf6
Anna Ridenour <annamridenour@gmail.com>
2019-04-15 08:57:24 -0700

    travis again

--- test/transformer.test.js
+++ test/transformer.test.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 describe('transformer function', () => {
     it('transforms the string in the file', () => {
-        return transformer(path.join(__dirname, '../lib/test-transform.txt' ))
+        return transformer(path.join(__dirname, '../lib/test-transform.txt'))
             .then(data => {
                 expect(data).toEqual('FDB');
             });
